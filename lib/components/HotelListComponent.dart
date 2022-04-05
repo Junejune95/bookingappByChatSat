f1aa64daa63c1293de0a8d400ad92a66a3f5402f
zune <seinmama95@gmail.com>
2022-04-05 12:25:40 +0630

    add hotel detail

--- lib/components/HotelListComponent.dart
+++ lib/components/HotelListComponent.dart
@@ -2,6 +2,7 @@
 
 import 'package:bookingapp/components/RatingComponent.dart';
 import 'package:bookingapp/models/BookingCommonModel.dart';
+import 'package:bookingapp/screen/BookingHotelDetailScreen.dart';
 import 'package:bookingapp/size_config.dart';
 import 'package:bookingapp/utils/BookingColors.dart';
 import 'package:bookingapp/utils/BookingWidgets.dart';
@@ -27,51 +28,53 @@ class HotelListComponent extends StatelessWidget {
       shrinkWrap: true,
       physics: NeverScrollableScrollPhysics(),
       itemBuilder: (BuildContext context, int index) {
-        return defaultCard(
-          margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-          child: Stack(
-            children: [
+        return InkWell(
+          onTap: () => {
+            BookingHotelDetailScreen().launch(context,
+                pageRouteAnimation: PageRouteAnimation.SlideBottomTop)
+          },
+          child: defaultCard(
+            margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            child: Stack(children: [
               Column(
-              // crossA xisAlignment: CrossAxisAlignment.start,
-              children: [
-                AspectRatio(
-                  aspectRatio: 3/2,
-                  child: Container(
-                    height: 200,
-                    width: SizeConfig.screenWidth,
-                 
-                    child: commonCacheImageWidget(hotellist[index].image, 0),
-                  ).cornerRadiusWithClipRRectOnly(topLeft: 10, topRight: 10),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          RatingComponent(
-                            isIndicator: true,
-                            rating: hotellist[index].rating,
-                          ),
-                          priceWrapper(
-                            price: hotellist[index].price,
-                            unit: 'night',
-                            isFullScreen: true,
-                          ),
-                        ],
-                      ),
-                      12.height,
-                      titleText(title: hotellist[index].name),
-                      12.height,
-                      locationWrapper(location: hotellist[index].location)
-                    ],
+                // crossA xisAlignment: CrossAxisAlignment.start,
+                children: [
+                  AspectRatio(
+                    aspectRatio: 3 / 2,
+                    child: Container(
+                      height: 200,
+                      width: SizeConfig.screenWidth,
+                      child: commonCacheImageWidget(hotellist[index].image, 0),
+                    ).cornerRadiusWithClipRRectOnly(topLeft: 10, topRight: 10),
                   ),
-
-                )
-              ],
-            ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            RatingComponent(
+                              isIndicator: true,
+                              rating: hotellist[index].rating,
+                            ),
+                            priceWrapper(
+                              price: hotellist[index].price,
+                              unit: 'night',
+                              isFullScreen: true,
+                            ),
+                          ],
+                        ),
+                        12.height,
+                        titleText(title: hotellist[index].name),
+                        12.height,
+                        locationWrapper(location: hotellist[index].location)
+                      ],
+                    ),
+                  )
+                ],
+              ),
               Positioned(
                   top: 15,
                   right: 15,
@@ -80,7 +83,7 @@ class HotelListComponent extends StatelessWidget {
                     color: Booking_TextColorWhite,
                     size: 24,
                   ).onTap(() {})),
-            ]
+            ]),
           ),
         );
       },
@@ -105,15 +108,14 @@ class HotelListComponent extends StatelessWidget {
                   Padding(
                     padding: const EdgeInsets.symmetric(vertical: 6),
                     child: AspectRatio(
-                      aspectRatio: 3/2,
+                      aspectRatio: 3 / 2,
                       child: Image.network(
-
                         hotellist[index].image,
                         fit: BoxFit.cover,
                       ).cornerRadiusWithClipRRect(8),
                     ),
                   ),
-                    10.height,
+                  10.height,
                   titleText(
                     title: hotellist[index].name,
                     size: 16,
--- lib/screen/BookingHotelDetailScreen.dart
+++ lib/screen/BookingHotelDetailScreen.dart
@@ -1,12 +1,30 @@
+import 'package:bookingapp/utils/BookingColors.dart';
+import 'package:bookingapp/utils/BookingWidgets.dart';
 import 'package:flutter/material.dart';
+import 'package:nb_utils/nb_utils.dart';
 
 class BookingHotelDetailScreen extends StatelessWidget {
-  const BookingHotelDetailScreen({ Key? key }) : super(key: key);
+  const BookingHotelDetailScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      backgroundColor: Booking_app_Background,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Container(
+              padding: EdgeInsets.all(10),
+              child: commonCacheImageWidget(
+                  'http://booking.qxlxt1pglq-xlm41kzlk3dy.p.runcloud.link/uploads/demo/hotel/gallery/hotel-gallery-1.jpg',
+                  240).cornerRadiusWithClipRRect(14)
+            ),
+            Container(
+
+            )
+          ],
+        ),
+      ),
     );
   }
 }
